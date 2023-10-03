16cd744e4c629ce875a250b91550cad58594f02a
S11788692 <tavionm21@gmail.com>
2023-10-03 11:58:04 -0400

    v0,1b

---
+++ gameProgramming/computer_science_exercises/collections.py
@@ -0,0 +1,15 @@
+# Collections Examples, Tavion Myers, v0.1a
+
+#LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
+breakfastfoods = ["Bacon","Waffles", "Pancakes","Cereal","Milk"]
+#each item on the list is known as an ELEMENT. 
+# The position in the list for each is the INDEX. 
+# The element "Bacon" is at index 0.
+#python Only: index -1 it is the last item on the list .
+testscore = [95, 10, 25, 55, 87, 34,]
+classgpa =[2.56, 3.67, 2,18, 3.63, 1.76, 2.99]
+
+# printing contents of an List 
+print (breakfastfoods)
+print (testscore)
+print (classgpa)
