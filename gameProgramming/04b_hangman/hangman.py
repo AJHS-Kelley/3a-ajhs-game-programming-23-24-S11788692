5fbecec43f74dce571fb57fee6b7df47131a61b2
S11788692 <tavionm21@gmail.com>
2023-11-01 12:05:39 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,57 @@
+import random
 wordlist = 'Butter,vest,coast,venmon,spiderman,joker,turtle,jacksonville,dogfood,pasta,baseball,dog,money,monkey,batman'.split()
 print(wordlist)
-
 print (wordlist[0])
+# VARIABLE_NAMES that are CAPS are contants and not meant to change. 
+HANGMAN_BOARD =['''
++----+
+     |
+     |
+     |
+     |
+     |
+   ======'''  '''
++----+
+O    |
+     |
+     |
+     |
+     |
+   ======'''  '''
++----+
+O    |    
+|    |
+     |
+     |
+   ======'''  '''
++----+
+O    |    
+|\   |
+     |
+     |
+   ======'''  '''
++----+
+ O   |    
+/|\  |
+     |
+     |
+   ======'''  '''
++----+
+ O   |    
+/|\  |
+/    |
+     |
+   ======'''  '''
++----+
+ O   |    
+/|\  |
+/ \  |
+     |
+   ======''']
+
+def get
+
+i = 0
+while i < len (HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i+=1
