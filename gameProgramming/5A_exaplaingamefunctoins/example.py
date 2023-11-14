51a1f22de29ca8dcfb31c9b65453dcc0dd473da4
S11788692 <tavionm21@gmail.com>
2023-11-14 13:14:31 -0500

    Update example.py

---
+++ gameProgramming/5A_exaplaingamefunctoins/example.py
@@ -0,0 +1,24 @@
+# Example Game Function, V0.0
+import random
+
+# Values 
+golfClubs = ["Putter","Wedge","Drives"]
+streghtwitClubs = 0 
+weather = 0 
+hittingSkill = 0
+
+
+def golfClubs():
+    pass 
+
+
+def streghtwitClubs():
+    pass
+
+
+def weather():
+    pass 
+
+
+def hittingSkill():
+    pass 
