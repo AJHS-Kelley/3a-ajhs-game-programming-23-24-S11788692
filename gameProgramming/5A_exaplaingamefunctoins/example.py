0aafa0584c2d1cb873bcd68a6a6f4d76573e4805
S11788692 <tavionm21@gmail.com>
2023-12-05 12:00:13 -0500

    Update example.py

--- gameProgramming/5A_exaplaingamefunctoins/example.py
+++ gameProgramming/5A_exaplaingamefunctoins/example.py
@@ -3,7 +3,7 @@ import random
 
 # Values 
 golfClubs = ["Putter","Wedge","Drives"]
-streghtwitClubs = 0 # Putter 5-25% 1-15yds, Wedge 26-65% 16-100yds Driver 66-100% 101-300yds
+PowerwitClubs = 0 # Putter 5-25% 1-15yds, Wedge 26-65% 16-100yds Driver 66-100% 101-300yds
 weather = None # Sunny, light Rain, Windy, Light snow
 hittingSkill = 0 # scale of 0% - 99%, 99% being the best \
 
@@ -13,7 +13,8 @@ def golfClubs(Putter, Wedge, Driver):
     pass 
 
 
-def streghtwitClubs():
+def PowerwitClubs():
+    if 1-15yds < 5-25%/
     pass
 
 
