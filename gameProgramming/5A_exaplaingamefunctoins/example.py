c752c1e0e83033a3a49286335790bb1b6f5355bd
S11788692 <tavionm21@gmail.com>
2023-12-07 12:04:11 -0500

    Update example.py

--- gameProgramming/5A_exaplaingamefunctoins/example.py
+++ gameProgramming/5A_exaplaingamefunctoins/example.py
@@ -6,7 +6,7 @@ golfClubs = ["Putter","Wedge","Drives"]
 PowerwitClubs = 0 # Putter 5-25% 1-15yds, Wedge 26-65% 16-100yds Driver 66-100% 101-300yds
 weather = ["Sunny", "Light Rain", "Heavy Rain", "light Fog"]
 windSpeed = ["light Wind","mild Wind","heavy Wind"]
-hittingSkill = 0 # scale of 0% - 99%, 99% being the best \
+Strokes = 0
 
 
 def golfClubs(Putter, Wedge, Driver): 
@@ -26,5 +26,5 @@ weather = random.randint (0,3)
     pass 
 
 
-def hittingSkill():
+def Strokes():
     pass 
