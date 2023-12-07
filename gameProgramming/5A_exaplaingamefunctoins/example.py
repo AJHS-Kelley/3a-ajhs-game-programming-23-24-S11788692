5898741d2b5d19cf14916623c26400806cd02359
S11788692 <tavionm21@gmail.com>
2023-12-07 13:08:08 -0500

    Update example.py

--- gameProgramming/5A_exaplaingamefunctoins/example.py
+++ gameProgramming/5A_exaplaingamefunctoins/example.py
@@ -6,7 +6,7 @@ golfClubs = ["Putter","Wedge","Drives"]
 PowerwitClubs = 0 # Putter 5-25% 1-15yds, Wedge 26-65% 16-100yds Driver 66-100% 101-300yds
 weather = ["Sunny", "Light Rain", "Heavy Rain", "light Fog"]
 windSpeed = ["light Wind","mild Wind","heavy Wind"]
-Strokes = 0
+Strokes = +
 
 
 def golfClubs(Putter, Wedge, Driver): 
@@ -27,4 +27,11 @@ weather = random.randint (0,3)
 
 
 def Strokes():
+    Strokes = (0, 12)
+    if you make it in 1 Stroke
+    print ("You made a Hole in 1!.")
+    if you make it in 2 Strokes
+    print("You made a Par")
+    if you make it in 3 Strokes
+    print ("You made a ")
     pass 
