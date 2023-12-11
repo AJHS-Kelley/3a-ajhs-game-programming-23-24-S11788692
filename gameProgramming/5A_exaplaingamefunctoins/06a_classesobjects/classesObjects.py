84e07f2e8938b3616cd2a10decc01da637251424
S11788692 <tavionm21@gmail.com>
2023-12-11 11:32:02 -0500

    Create classesObjects.py

---
+++ gameProgramming/5A_exaplaingamefunctoins/06a_classesobjects/classesObjects.py
@@ -0,0 +1,9 @@
+
+class Person # Class names should be pascalc ase
+    def __init__(self, age, height, name, weight, birthday )
+    # The self keyword refers to the speicfic object you are dealing with.
+    self.age = age 
+    self.height = height
+    self.name = name 
+    self.weight = weight 
+    self. birthday
