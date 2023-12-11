fbfddedaa713151f022f01b1a9018e3b173973a2
S11788692 <tavionm21@gmail.com>
2023-12-11 11:40:06 -0500

    Update classesObjects.py

--- gameProgramming/5A_exaplaingamefunctoins/06a_classesobjects/classesObjects.py
+++ gameProgramming/5A_exaplaingamefunctoins/06a_classesobjects/classesObjects.py
@@ -1,9 +1,15 @@
 
-class Person # Class names should be pascalc ase
+class Person # Class names should be pascalcase
     def __init__(self, age, height, name, weight, birthday )
     # The self keyword refers to the speicfic object you are dealing with.
     self.age = age 
     self.height = height
     self.name = name 
     self.weight = weight 
-    self. birthday
+    self.birthday = birthday 
+
+    # A class is a 'blueprint' to make a object. 
+
+    examplePerson = Person(17, "5'10\'" "Tay" 250, March 9)
+examplePerson1 = Person (24, "4'7")
+    print(examplePerson)
