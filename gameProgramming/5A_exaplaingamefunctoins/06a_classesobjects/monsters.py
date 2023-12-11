2b146a51d6bd2a2b667d539cacf7429ff8ce8a24
S11788692 <tavionm21@gmail.com>
2023-12-11 13:05:55 -0500

    Create monsters.py

---
+++ gameProgramming/5A_exaplaingamefunctoins/06a_classesobjects/monsters.py
@@ -0,0 +1,12 @@
+
+class Monster:
+    def __init__(self, Speed, Damage, Color)
+        self.Speed = Speed
+        self.Damage = Damage 
+        self.color = Color
+
+
+    def __str__(self):
+        return f"Monster Name{self.Speed}\n{self.Damage}\n{self.Color}\n"
+
+
